8c584389c175d5eb4b29040bbc3ef7c77e10d094
IstiN <istin2007@gmail.com>
2025-01-16 12:47:28 +0300

    1.4.46: DI fixed for TC generator

---
+++ src/main/java/com/github/istin/dmtools/di/TestCasesGeneratorComponent.java
@@ -0,0 +1,12 @@
+package com.github.istin.dmtools.di;
+
+import com.github.istin.dmtools.qa.TestCasesGenerator;
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+@Singleton
+@Component(modules = {JiraModule.class, AIComponentsModule.class, ConfluenceModule.class, AIAgentsModule.class})
+public interface TestCasesGeneratorComponent {
+    void inject(TestCasesGenerator testCasesGenerator);
+}
