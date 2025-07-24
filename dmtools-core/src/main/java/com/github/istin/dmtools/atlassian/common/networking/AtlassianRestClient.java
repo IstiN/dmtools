118c9bf278d8abc54aaa7fef646dcab0a9c11567
IstiN <istin2007@gmail.com>
2025-07-25 00:08:12 +0300

    DMC-85 fix extension

--- dmtools-core/src/main/java/com/github/istin/dmtools/atlassian/common/networking/AtlassianRestClient.java
+++ dmtools-core/src/main/java/com/github/istin/dmtools/atlassian/common/networking/AtlassianRestClient.java
@@ -1,11 +1,17 @@
 package com.github.istin.dmtools.atlassian.common.networking;
 
 import com.github.istin.dmtools.networking.AbstractRestClient;
+import lombok.Getter;
+import lombok.Setter;
 import okhttp3.Request;
 
 import java.io.IOException;
 
 public abstract class AtlassianRestClient extends AbstractRestClient {
+
+    @Setter
+    @Getter
+    private String authType = "Basic";
 
     public AtlassianRestClient(String basePath, String authorization) throws IOException {
         super(basePath, authorization);
@@ -14,7 +20,7 @@ public abstract class AtlassianRestClient extends AbstractRestClient {
     @Override
     public Request.Builder sign(Request.Builder builder) {
         return builder
-                .header("Authorization", authorization)
+                .header("Authorization", authType + " " +authorization)
                 .header("X-Atlassian-Token", "nocheck")
                 .header("Content-Type", "application/json")
                 ;
