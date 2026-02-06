b0b6bc2c957f3ddc71f1b83eba396d0c5575ac64
AI Teammate <agent.ai.native@gmail.com>
2026-02-06 09:21:37 +0300

    Update reporting core classes with analytics and caching

--- dmtools-core/src/main/java/com/github/istin/dmtools/report/productivity/QAProductivityReport.java
+++ dmtools-core/src/main/java/com/github/istin/dmtools/report/productivity/QAProductivityReport.java
@@ -2,7 +2,6 @@ package com.github.istin.dmtools.report.productivity;
 
 import com.github.istin.dmtools.ai.AI;
 import com.github.istin.dmtools.atlassian.jira.BasicJiraClient;
-import com.github.istin.dmtools.atlassian.jira.model.IssueType;
 import com.github.istin.dmtools.atlassian.jira.utils.ChangelogAssessment;
 import com.github.istin.dmtools.common.model.ITicket;
 import com.github.istin.dmtools.common.timeline.Release;
@@ -19,7 +18,6 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -52,7 +50,6 @@ public class QAProductivityReport extends AbstractJob<QAProductivityReportParams
         movedToReopened(qaProductivityReportParams, listOfCustomMetrics);
         createdTests(qaProductivityReportParams, listOfCustomMetrics);
         fieldsChanged(qaProductivityReportParams, listOfCustomMetrics);
-        ticketLinksChanged(qaProductivityReportParams, listOfCustomMetrics);
         commentsWritten(qaProductivityReportParams, listOfCustomMetrics);
         ProductivityUtils.vacationDays(listOfCustomMetrics, Employees.getTesters(qaProductivityReportParams.getEmployees()));
 //        numberOfRejectedBugs(qaProductivityReportParams, listOfCustomMetrics);
@@ -60,19 +57,6 @@ public class QAProductivityReport extends AbstractJob<QAProductivityReportParams
     }
 
     //
-
-    protected void ticketLinksChanged(QAProductivityReportParams qaProductivityReportParams, List<Metric> listOfCustomMetrics) {
-        listOfCustomMetrics.add(new Metric("Test Ticket Linked", qaProductivityReportParams.isWeight(), new TicketFieldsChangesRule(Employees.getTesters(qaProductivityReportParams.getEmployees()), new String[]{"link"}, false, false) {
-            @Override
-            public List<KeyTime> check(TrackerClient trackerClient, ITicket ticket) throws Exception {
-                if (IssueType.isTestCase(ticket.getIssueType())) {
-                    return super.check(trackerClient, ticket);
-                } else {
-                    return Collections.emptyList();
-                }
-            }
-        }));
-    }
 
     protected void fieldsChanged(QAProductivityReportParams qaProductivityReportParams, List<Metric> listOfCustomMetrics) {
         listOfCustomMetrics.add(new Metric("Ticket Fields Changed", qaProductivityReportParams.isWeight(), new TicketFieldsChangesRule(Employees.getTesters(qaProductivityReportParams.getEmployees()))));
